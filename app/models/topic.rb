7d1cb19471231c8aca2ab98b79afe04c9f00e8f8
Diego Gomez <diego.gomez@codescrum.com>
2013-12-13 11:36:45 -0500

    add timestamp to Topic Model and add two methods for know the post number for current Topic and the date of the last post

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,21 @@
 class Topic
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title, type: String
   field :description, type: String
 
   belongs_to :user
   has_many :posts
+
+  # Class Methods
+
+  # @return [Integer] with the number of posts in the current Topic
+  def post_number
+    self.posts.count
+  end
+
+  # @return [Date] with the date of the last post
+  def last_post
+    self.posts.last.try(:created_at)
+  end
 end
